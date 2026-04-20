08606decef74d591a503a6141a91cf40e39cd47c
jarrietasanagustin <jorge.arrieta@uib.es>
2026-04-20 15:19:03 +0200

    Update active_nematics.py

--- active_nematics.py
+++ active_nematics.py
@@ -23,10 +23,55 @@ ybasis = d3.ComplexFourier(coord['y'], size=Ny, bounds=(0, Ly), dealias=dealias)
 
 #We now define the fields tha we need to calculate. The velocity field and the orientation field that his a tensor
 
-u = dist.VectorField(coord, name='u', bases=(xbasis,ybasis)) #velcoity field
+u = dist.ScalarField( name='u', bases=(xbasis,ybasis)) #x component of the velcoity field
+v = dist.ScalarField( name='v', bases=(xbasis,ybasis)) #y component of the velcoity field
 #Q=dist.TensorField(coords,name='Q',bases=(xbasis,ybasis),components=2)# we comment this it will be solved later
 #Rembmer Q is symmetric and traceless we define first as scalar fields
 Qxx=dist.ScalarField(name='Qxx',bases=(xbasis,ybasis))
 Qyy=dist.ScalarField(name='Qyy',bases=(xbasis,ybasis))
 Qyy=-Qxx
 Qxy=dist.ScalarField(name='Qxy',bases=(xbasis,ybasis))
+
+
+#we define the derivatives of the components of the velocity to assemble the strain tensor 
+ux=d3.Differentiate(u,coord['x'])
+uy=d3.Differentiate(u,coord['y'])
+vx=d3.Differentiate(v,coord['x'])
+vy=d3.Differentiate(v,coord['y'])
+#Strain rate tensor
+Exx=ux
+Exy=1/2*(uy+vx)
+Eyy=vy
+#vorticity
+Omega=1/2*(uy-vx)
+Omegaxx=0
+Omegayy=0
+Omegaxy=Omega
+Omegayx=-Omega
+
+lambda=1#tumbling parameter
+
+#A=lambda*E+Omega
+A11=lambda*Exx+Omegaxx
+A12=lambda*Exy+Omegaxy
+A21=lambda*Exy+Omegayx
+A22=lambda*Eyy+Omegayy
+#B=Q+I/3
+B11=Qxx+1/3
+B12=Qxy
+B21=Qxy
+B22=Qyy+1/3
+
+#C=lambda*E-Omega
+C11=lambda*Exx-Omegaxx
+C12=lambda*Exy-Omegaxy
+C21=lambda*Exy-Omegayx
+C22=lambda*Eyy-Omegayy
+#D=Q:nabla u
+D=Qxx*(ux-vy)+Qyy*(uy+vx)
+####################################################################
+#We now write explicit expression for S11 and S12
+####################################################################
+
+S11=(A11*B11+A12*B21)+(B11*C11+B12*C21)-2*lambda*B11*D
+S12=A11*B12+A12*B22+(B11*C21+B12*C22)-2*lambda*B12*D
