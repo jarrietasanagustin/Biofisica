136512c6785ad45918acb7e143f1b4b077928d02
jarrietasanagustin <jorge.arrieta@uib.es>
2026-04-23 13:12:18 +0200

    Working version
    2D periodic active nematics code running.
    Results need to be checked.

--- active_nematics.py
+++ active_nematics.py
@@ -20,33 +20,32 @@ coord=d3.CartesianCoordinates('x','y')
 dist = d3.Distributor(coord, dtype=np.complex128)
 dealias = 3/2
 xbasis = d3.ComplexFourier(coord['x'], size=Nx, bounds=(0, Lx), dealias=dealias)
-ybasis = d3.ChebyshevT(coord['y'],     size=Ny, bounds=(0, Ly), dealias=dealias)
+ybasis = d3.ComplexFourier(coord['y'],     size=Ny, bounds=(0, Ly), dealias=dealias)
 #xbasis = d3.ComplexFourier(coord['x'], size=Nx, bounds=(0, Lx), dealias=dealias)
 
 #ybasis = d3.ComplexFourier(coord['y'], size=Ny, bounds=(0, Ly), dealias=dealias)
 
 #We now define the fields tha we need to calculate. The velocity field and the orientation field that his a tensor
 
-u = dist.ScalarField( name='u', bases=(xbasis,ybasis)) #x component of the velcoity field
-v = dist.ScalarField( name='v', bases=(xbasis,ybasis)) #y component of the velcoity field
-p = dist.ScalarField( name='p', bases=(xbasis,ybasis)) #pressure field
-tau_p = dist.Field(name='tau_p')
+psi = dist.Field(name='psi', bases=(xbasis, ybasis))  # u=dy(psi), v=-dx(psi)
 #Q=dist.TensorField(coords,name='Q',bases=(xbasis,ybasis),components=2)# we comment this it will be solved later
+tau_psi = dist.Field(name='tau_psi')
 
 #Rembmer Q is symmetric and traceless we define first as scalar fields
-Qxx=dist.ScalarField(name='Qxx',bases=(xbasis,ybasis))
-Qyy=dist.ScalarField(name='Qyy',bases=(xbasis,ybasis))
+Qxx=dist.Field(name='Qxx',bases=(xbasis,ybasis))
+
 #Qyy=-Qxx
 Qxy=dist.ScalarField(name='Qxy',bases=(xbasis,ybasis))
 
-
+dx = lambda A: d3.Differentiate(A, coord['x'])
+dy = lambda A: d3.Differentiate(A, coord['y'])
+u = dy(psi)
+v = -dx(psi)
 #we define the derivatives of the components of the velocity to assemble the strain tensor 
 ux=d3.Differentiate(u,coord['x'])
 uy=d3.Differentiate(u,coord['y'])
 vx=d3.Differentiate(v,coord['x'])
 vy=d3.Differentiate(v,coord['y'])
-dx = lambda A: d3.Differentiate(A, coord['x'])
-dy = lambda A: d3.Differentiate(A, coord['y'])
 
 #Strain rate tensor
 Exx=ux
@@ -79,6 +78,7 @@ C21=lambda_n*Exy-Omegayx
 C22=lambda_n*Eyy-Omegayy
 #D=Q:nabla u
 D=Qxx*(ux-vy)+Qxy*(uy+vx)
+curl_f = dx(dx(Qxy)) - dx(dy(Qxx)) - dy(dx(Qxx)) - dy(dy(Qxy))
 ####################################################################
 #We now write explicit expression for S11 and S12
 ####################################################################
@@ -93,42 +93,35 @@ lap = lambda A: dx(dx(A)) + dy(dy(A))
 # active force
 fx = dx(Qxx) + dy(Qxy)
 fy = dx(Qxy) - dy(Qxx)
-problem = d3.IVP([u,v,p,Qxx,Qxy,tau_p], namespace=locals())
+problem = d3.IVP([psi,Qxx,Qxy,tau_psi], namespace=locals())
 #equation for the components of the nematic tensor field
 problem.add_equation("dt(Qxx)=-u*dx(Qxx)-v*dy(Qxx)+S11+1/Pe*(dx(dx(Qxx))+dy(dy(Qxx)))")
 problem.add_equation("dt(Qxy)=-u*dx(Qxy)-v*dy(Qxy)+S12+ 1/Pe*(dx(dx(Qxy))+dy(dy(Qxy)))")
 #continuity equation for the velocity field
-problem.add_equation("dx(u)+dy(v)+tau_p=0")
-#we need to add the momentum equation for the velocity field
-problem.add_equation("integ(p) = 0")   # <-- fixes the pressure gauge
-problem.add_equation("dx(p)-(lap(u)-fx)=0")
-problem.add_equation("dy(p)-(lap(v) +fy)=0")
-
+problem.add_equation("lap(lap(psi)) - curl_f +tau_psi= 0")
+problem.add_equation("integ(psi) = 0")
 #we define the initial conditions for the fields
 # Small random perturbation around isotropic state
 
 # --- Initial conditions ---
 Qxx['g'] = 0.001 * np.random.randn(Nx, Ny)
 Qxy['g'] = 0.001 * np.random.randn(Nx, Ny)
-u['g']   = 0.0
-v['g']   = 0.0
-p['g']   = 0.0
-
+psi['g'] = 0.0
 # --- Solver ---
 solver = problem.build_solver(d3.RK443)
-solver.stop_sim_time = 500
-timestep = 0.001
+solver.stop_sim_time = 20
+timestep = 0.01
 
 # --- File handler (MUST be before the time loop) ---
 snapshots = solver.evaluator.add_file_handler('snapshots', iter=100)
 snapshots.add_task(Qxx, name='Qxx')
 snapshots.add_task(Qxy, name='Qxy')
-snapshots.add_task(u,   name='u')
-snapshots.add_task(v,   name='v')
-snapshots.add_task(p,   name='p')
+snapshots.add_task( dy(psi), name='u')
+snapshots.add_task(-dx(psi), name='v')
+snapshots.add_task(psi,      name='psi')
 
 # --- Time loop ---
 while solver.proceed:
     solver.step(timestep)
     if solver.iteration % 100 == 0:
-        print(f"t = {solver.sim_time:.3f}")
+            print(f"t = {solver.sim_time:.3f}")
